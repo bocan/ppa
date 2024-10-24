4ac114f4211526e719fd397e99274dc1bbda1e37
Chris Funderburg <chris@funderburg.me>
2024-10-24 09:45:53 +0000

    fixes

--- build.sh
+++ build.sh
@@ -7,13 +7,13 @@
 apt install apt-utils devscripts debsigs
 debsign *changes
 debsigs --sign=origin  *deb
-
-## TO BE DECIDED:
 dpkg-scanpackages -a amd64 amd64  > Packages
-dpkg-scansources amd64 > Sources
-
+dpkg-scansources > Sources
 gzip -k -f Packages
 gzip -k -f Sources
+
+## TO BE DECIDED:
+
 
 apt-ftparchive release . > Release
 
@@ -29,6 +29,8 @@ KEYNAME="chris@funderburg.me"
 rm -fr Release.gpg; gpg --default-key ${KEYNAME} -abs -o Release.gpg Release
 rm -fr InRelease; gpg --default-key ${KEYNAME} --clearsign -o InRelease Release
 
+rm -fr Sources.gpg; gpg --default-key ${KEYNAME} -abs -o Sources.gpg Sources
+#rm -fr InRelease; gpg --default-key ${KEYNAME} --clearsign -o InRelease Release
 
     curl -sSfL https://bocan.github.io/ppa/KEY.gpg | gpg --dearmor >/etc/apt/trusted.gpg.d/rspamd.gpg
     echo \
